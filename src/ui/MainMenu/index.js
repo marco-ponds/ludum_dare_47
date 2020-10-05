a5c8cd2738b491d5bd8042f84c9486322ff523c5
DJHunn39 <dannyjohnhunn@yahoo.co.uk>
2020-10-05 19:39:15 +0100

    Add instructions

--- src/ui/MainMenu/index.js
+++ src/ui/MainMenu/index.js
@@ -1,7 +1,7 @@
-import { playStartSound } from "../../level/sounds";
+import { playStartSound } from '../../level/sounds';
+import InstructionDetails from './InstructionDetails';
 
 const MainMenu = ({ onStart }) => {
-    
     const handleClick = () => {
         playStartSound();
         onStart();
@@ -9,16 +9,22 @@ const MainMenu = ({ onStart }) => {
 
     return (
         <div className="main-menu fancy-text">
-            <div className='menu-container box'>
+            <div className="menu-container box">
                 <h1 className="game-title">Ferrovia Folle</h1>
                 <h2 className="game-subtitle">(Crazy Railway)</h2>
-                <p className="game-instructions">Instructions go here</p>
+                <div className="game-instructions">
+                    <p>
+                        Choo, choo! The train is moving, and it's your job to
+                        keep it that way!
+                    </p>
+                    <InstructionDetails />
+                </div>
                 <button className="button" onClick={handleClick}>
                     Start
                 </button>
             </div>
         </div>
     );
-}
+};
 
 export default MainMenu;
